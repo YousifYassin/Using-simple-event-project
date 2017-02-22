574b1b64975e819ff9c717b70b1a0c9eab8e9a33
snipe <snipe@RAIDEN>
2017-02-22 15:23:27 +0300

    Add project files.

---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    public delegate void myEventHandler(string value);
+
+    class EventPublisher
+    {
+        private string theval;
+        public event myEventHandler changedValue;
+        public string Val
+        {
+            set
+            {
+                this.theval = value;
+                this.changedValue(theval);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventPublisher obj = new EventPublisher();
+            obj.changedValue += changedValue;
+            string str;
+            do
+            {
+                Console.WriteLine("Enter the value");
+                str = Console.ReadLine();
+                if (!str.Equals("exit"))
+                {
+                    obj.Val = str;
+                }
+            } while (!str.Equals("exit"));
+            Console.WriteLine("Goodbye!");
+        }
+        static void changedValue(string value)
+        {
+            Console.WriteLine("The value changed to {0}", value);
+        }
+    }
+}
